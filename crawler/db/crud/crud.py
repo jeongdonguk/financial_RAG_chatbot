6d8fc6daa0f03c2fcc4b263c738e12b0a72566c0
donguk <donguk1221@naver.com>
2025-06-06 17:20:00 +0900

    DEBUG : db연결실패 디버깅

--- crawler/db/crud/crud.py
+++ crawler/db/crud/crud.py
@@ -1,6 +1,6 @@
 # 기본 셀렉트 관련 쿼리 사용을 위한 파일
 from typing import Any, List, Optional, Dict
-from crawler.core.database import Base
+from core.database import Base
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import and_, or_, not_, func, select
 
